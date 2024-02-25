4a3a2f89bd12e1997ce176846decfba3d1148550
origin <mambo771@mail.ru>
2024-02-25 22:24:32 +0300

    Initial commit

---
+++ src/main/java/sets/employeewebapp/EmployeeWebAppApplication.java
@@ -0,0 +1,13 @@
+package sets.employeewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sets/employeewebapp/EmployeeWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package sets.employeewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
